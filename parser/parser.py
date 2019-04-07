88505df2fc7a5aceac77c4465cb3e8bd3a73e72b
Hsin-Cheng Chao <olala7846@gmail.com>
2019-04-07 12:07:37 -0600

    Simple parser

--- parser/parser.py
+++ parser/parser.py
@@ -3,3 +3,15 @@
 # for reason, see the following link
 # https://docs.scrapy.org/en/latest/topics/selectors.html#scrapy.selector.Selector
 
+from lxml import etree  # Element Tree
+
+FILE_PATH = 'resource/chinatimes_sample.html'
+
+with open(FILE_PATH, 'r') as f:
+  parser = etree.XMLParser()
+  html_text = f.read()
+  root = etree.parse(html_text, parser)
+  print(root)
+
+  #TODO(hcchao): draw tree node
+
