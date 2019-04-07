9ff0075db255d71af2f075fe67ced5a9df8b4226
Hsin-Cheng Chao <olala7846@gmail.com>
2019-04-07 11:47:48 -0600

    Create spider/ parser/ structure
    Reasons:
    1. We may want to keep the raw html files
    2. We can use lxml instead of scrapy selector to parse page

---
+++ parser/parser.py
@@ -0,0 +1,5 @@
+# parser.py
+# We parse html page useing lxml instead of scrapy selector
+# for reason, see the following link
+# https://docs.scrapy.org/en/latest/topics/selectors.html#scrapy.selector.Selector
+
---
+++ spider/spider.py
@@ -0,0 +1,5 @@
+# TODO(hcchao): create spider
+
+# Spider sould be only used to recursively download htmls
+# Parsing tasks of each news page are handled in /parser/
+
