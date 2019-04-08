9f138e7d3d10ccb01675b7c04035dd67afd9b3cc
Hsin-Cheng Chao <olala7846@gmail.com>
2019-04-07 22:01:11 -0600

    Add UDN support

--- parser/parser.py
+++ parser/parser.py
@@ -5,8 +5,19 @@
 
 from lxml import etree  # Element Tree
 
-FILE_PATH = 'resource/chinatimes_sample.html'
-IGNORE_TAGS = {'script', 'meta', 'iframe'}
+# FILE_PATH = 'resource/chinatimes_sample.html'
+FILE_PATH = 'resource/udn.html'
+IGNORE_TAGS = {
+  'audio',
+  'button',
+  'iframe',
+  'meta',
+  'nav',
+  'script',
+  'select',
+  'style',
+  'video',
+}
 IGNORE_CLASSES = {
   # 中時
   'comments',
@@ -15,7 +26,31 @@ IGNORE_CLASSES = {
   'popin-recommend',
   'recommended-article',
   'social-share',
+  # 聯合
+  'listing',
+  'menu',
+  'only_web',
+  'only_mobile',
+  'social_bar',
+  'social_more',
+  'sitemap',
 }
+IGNORE_IDS = {
+  # 聯合
+  'browser-update',
+  'comments',
+  'ec',
+  'ec-body',
+  'footer',
+  'header',
+  'msg',
+  'next',
+  'prev',
+  'sidebar',
+  'sidebar_attention_body',
+  'toprow',
+}
+
 SECTION_TAGS = {'title', 'header', 'figure'}
 SPACE_CHARS = {'\n', '\r', '\t', '\v', ' '}
 
@@ -25,7 +60,6 @@ def no_newline(string):
   return string
 
 with open(FILE_PATH, 'r') as f:
-  # recover=True ignores attribute without value
   parser = etree.HTMLParser()
   html_text = f.read()
   root = etree.HTML(html_text, parser)
@@ -48,14 +82,18 @@ with open(FILE_PATH, 'r') as f:
       if node_classes.intersection(IGNORE_CLASSES):
         return
 
+    # Skip recommened article
+    if 'id' in node.attrib and node.attrib['id'] in IGNORE_IDS:
+      return
+
     if node.text:
       html_text = html_text + no_newline(node.text)
-      for sub_node in node:
-        _traverse(sub_node)
-        if sub_node.tail:
-          html_text = html_text + no_newline(sub_node.tail)
-      if node.tag in SECTION_TAGS:
-        html_text += '|'
+    for sub_node in node:
+      _traverse(sub_node)
+      if sub_node.tail:
+        html_text = html_text + no_newline(sub_node.tail)
+    if node.tag in SECTION_TAGS:
+      html_text += '|'
 
   _traverse(root)
   print(html_text)
